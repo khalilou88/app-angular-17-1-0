9e44f6a13136ad094457c42b34ed159405ea760c
khalilou88 <32600911+khalilou88@users.noreply.github.com>
2024-11-26 15:09:13 +0100

    add @semantic-components/heroicons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+
+import { SvgRocketLaunchIcon } from '@semantic-components/heroicons/24/solid';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, SvgRocketLaunchIcon],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  encapsulation: ViewEncapsulation.None,
 })
 export class AppComponent {
   title = 'app-angular-17-1-0';
